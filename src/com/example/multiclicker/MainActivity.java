e4fb083030bba69de66333260d319b2c93cac257
chickendiver <bradley.poulette@gmail.com>
2014-01-17 14:48:39 -0700

    Trial for how to handle button presses, as well as where to show text.

--- src/com/example/multiclicker/MainActivity.java
+++ src/com/example/multiclicker/MainActivity.java
@@ -1,22 +1,44 @@
 package com.example.multiclicker;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+	
+	int counter = 0;
+	Button btn1;
+	EditText counterText;
+	
+	//Jan. 17th : http://simpledeveloper.com/how-to-build-simple-counter-android-app/
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		btn1 = (Button)findViewById(R.id.button_text);
+		counterText = (EditText)findViewById(R.id.text_view_1);
+		
+		btn1.setOnClickListener(this);
+		
 	}
 
-	@Override
+	/*@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}*/
+	
+	@Override
+	public void onClick(View v) {
+		if (v == btn1){ 
+			counter++;
+			counterText.setText(Integer.toString(counter));
+		}
 	}
 
 }
