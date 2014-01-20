e9e7f3b83058ef3c644256d87d43a0142723992c
chickendiver <bradley.poulette@gmail.com>
2014-01-19 17:31:24 -0700

    Fiddled around in counter class, added getters and setters

--- src/com/example/multiclicker/Counter.java
+++ src/com/example/multiclicker/Counter.java
@@ -1,5 +1,30 @@
 package com.example.multiclicker;
 
 public class Counter {
+	
+	protected String counterName;
+    protected int counterValue;
+ 
+    // Constructor
+    public Counter(String counterName, int counterValue) {
+        this.counterName = counterName;
+        this.counterValue= counterValue;
+    }
+    
+    public String getCounterName() {
+		return counterName;
+	}
+
+	public void setCounterName(String counterName) {
+		this.counterName = counterName;
+	}
+
+	public int getCounterValue() {
+		return counterValue;
+	}
+
+	public void setCounterValue(int counterValue) {
+		this.counterValue = counterValue;
+	}
 
 }
