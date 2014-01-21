daa37422bb42b7895a62d014f01a703bc828c481
chickendiver <bradley.poulette@gmail.com>
2014-01-21 16:32:50 -0700

    Did work on Counter and added CounterAdapter

---
+++ src/com/example/multiclicker/CounterAdapter.java
@@ -0,0 +1,60 @@
+package com.example.multiclicker;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+// Based largely on article by Pete Houston, accessed on Jan. 21st at:
+// http://xjaphx.wordpress.com/2011/06/11/create-a-simple-phone-book/
+public class CounterAdapter extends BaseAdapter{
+	private Context context;
+	private List<Counter> counterList;
+	
+	public CounterAdapter(Context context, List<Counter> counterList) {
+		this.context = context;
+		this.counterList = counterList;
+	}
+	
+	@Override
+	public int getCount() {
+		return counterList.size();
+	}
+	
+	@Override
+	public Object getItem(int pos){
+		return counterList.get(pos);
+	}
+	
+	@Override
+	public long getItemId(int pos) {
+		return pos;
+	}
+	
+	@Override
+	public View getView(int pos, View convertView, ViewGroup parent) {
+		// Use the proper counter
+		Counter counterInstance = counterList.get(pos);
+		
+		// Inflate list view layout if null (copied exactly)
+		if(convertView == null){
+			LayoutInflater inflater = LayoutInflater.from(context);
+			convertView = inflater.inflate(R.layout.counter_layout, null);
+		}
+		
+		// set counter's button
+		Button counterbutton = (Button) convertView.findViewById(R.id.button_text);
+		counterbutton.setOnClickListener();
+		
+		// set counter's textView
+		TextView counterCount = (TextView)convertView.findViewById(R.id.text_view_1);
+		counterCount.setText(counterInstance.getCounterValue());
+		
+		return convertView;
+	}
+}
